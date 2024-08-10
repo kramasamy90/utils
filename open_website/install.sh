fcdbdd01cec8306d7c1bc8a2b0b87957d3ff1652
Ramasamy Kandasamy <kramasamy90@gmail.com>
2024-08-10 23:22:29 +0530

    Rename openweb -> open_website and associated refactoring.

--- open_website/install.sh
+++ open_website/install.sh
@@ -5,29 +5,29 @@ if [ ! -d ~/.local/opt ]; then
     mkdir -p ~/.local/opt
 fi
 
-# Create directory ~/.local/opt/openweb
-if [ ! -d ~/.local/opt/openweb ]; then
-    mkdir -p ~/.local/opt/openweb
+# Create directory ~/.local/opt/open_website
+if [ ! -d ~/.local/opt/open_website ]; then
+    mkdir -p ~/.local/opt/open_website
 fi
 
 # Create directory ~/.local/opt/open/data
-if [ ! -d ~/.local/opt/openweb/data ]; then
-    mkdir -p ~/.local/opt/openweb/data
+if [ ! -d ~/.local/opt/open_website/data ]; then
+    mkdir -p ~/.local/opt/open_website/data
 fi
 
 # Create directory ~/.local/opt/open/src
-if [ ! -d ~/.local/opt/openweb/src ]; then
-    mkdir -p ~/.local/opt/openweb/src
+if [ ! -d ~/.local/opt/open_website/src ]; then
+    mkdir -p ~/.local/opt/open_website/src
 fi
 
 # Copy files to ~/.local/opt/open
-rsync -av ./src/ ~/.local/opt/openweb/src/
-rsync -av ./data/ ~/.local/opt/openweb/data/
+rsync -av ./src/ ~/.local/opt/open_website/src/
+rsync -av ./data/ ~/.local/opt/open_website/data/
 
 
 # Add the path to .bashrc
 if [ -f ~/.bashrc ]; then
-    if ! grep -q "export PATH=\$PATH:~/.local/opt/openweb/src" ~/.bashrc; then
-        echo "export PATH=\$PATH:~/.local/opt/openweb/src" >> ~/.bashrc
+    if ! grep -q "export PATH=\$PATH:~/.local/opt/open_website/src" ~/.bashrc; then
+        echo "export PATH=\$PATH:~/.local/opt/open_website/src" >> ~/.bashrc
     fi
 fi
