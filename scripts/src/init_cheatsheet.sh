dffa044bfba146820e34bcc8f309fd19c6ce888c
Ramasamy Kandasamy <kramasamy90@gmail.com>
2024-08-19 23:11:14 +0530

    Change script file to reflect change in template dir organization.

--- scripts/src/init_cheatsheet.sh
+++ scripts/src/init_cheatsheet.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
 # This script is used to initialize the cheatsheet
-template_dir="/mnt/e/Documents/Templates/LaTeX Templates/Cheatsheet"
+template_dir="/home/kramasamy/Documents/Templates/LaTeX Templates/Cheatsheet/"
 
-if [ ! -d "src" ]; then
-    mkdir src
-fi
-
-
-cp "$template_dir"/cheatsheet_main.tex main.tex
-cp "$template_dir"/cheatsheet_preamble.tex src/preamble.tex
+rsync -av "$template_dir"  ./
