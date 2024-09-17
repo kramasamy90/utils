9e7e4ba3985f0a4841a031a6b9cbfbd762a37b3b
Ramasamy Kandasamy <kramasamy90@gmail.com>
2024-09-17 21:56:35 +0530

    Update `init.py`.

--- init.py
+++ init.py
@@ -11,18 +11,18 @@ directory.
 '''
 
 # Get directory info. 
-REPO_DIR = os.path.dirname(__file__)
+REPO_DIR = os.path.dirname(os.path.abspath(__file__))
 CONFIG_FILE = os.path.join(REPO_DIR, 'config.yaml')
 with open(CONFIG_FILE, 'r') as file:
     configs = yaml.safe_load(file)
 INSTALL_DIR = os.path.realpath(os.path.expanduser(configs['install_dir']))
 
-# Save config.yaml.
-configs['repo_dir'] = REPO_DIR
-with open(CONFIG_FILE, 'w') as file:
-    yaml.dump(configs, file)
-
-# Create directories and move files.
+# Create installation directory.
 if not os.path.exists(INSTALL_DIR):
     os.makedirs(INSTALL_DIR)
-os.system(f"cp config.yaml {INSTALL_DIR}/config.yaml")
+
+# Update and save config.yaml.
+TARGET_CONFIG_FILE = os.path.join(INSTALL_DIR, 'config.yaml')
+configs['repo_dir'] = REPO_DIR
+with open(TARGET_CONFIG_FILE, 'w') as file:
+    yaml.dump(configs, file)
