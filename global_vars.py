75f5b85d3a884115444fa4c133cbc21bf034f925
Ramasamy Kandasamy <kramasamy90@gmail.com>
2024-09-21 23:16:06 +0530

    Update `global_vars.py`.

--- global_vars.py
+++ global_vars.py
@@ -1,2 +1,9 @@
-INSTALL_DIR = '~/.local/opt/utils'
-DATA_DIR = '~/Code/data'
+import os
+
+# Set the below variables.
+_INSTALL_DIR = '~/.local/opt/utils'
+_DATA_DIR = '~/Code/data'
+
+# Don't touch the below code.
+INSTALL_DIR = os.path.realpath(os.path.expanduser(_INSTALL_DIR))
+DATA_DIR = os.path.realpath(os.path.expanduser(_DATA_DIR))
