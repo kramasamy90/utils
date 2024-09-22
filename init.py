275b8bf55c200a85ad60005c1645371458fe4772
Ramasamy Kandasamy <kramasamy90@gmail.com>
2024-09-22 17:31:46 +0530

    Add global variable to an yaml file.

--- init.py
+++ init.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import yaml
 
 FILE_DIR = os.path.abspath(os.path.dirname(__file__))
 
@@ -17,12 +18,17 @@ directory.
 # Get directory info. 
 REPO_DIR = os.path.dirname(os.path.abspath(__file__))
 
+# Write INSTALL_DIR and CACHE_DIR to yaml file.
+with open(f"{REPO_DIR}/config.yaml", "w") as f:
+    yaml.dump({"INSTALL_DIR": INSTALL_DIR, "CACHE_DIR": CACHE_DIR}, f)
+
 # Create installation directory.
 if not os.path.exists(INSTALL_DIR):
     os.makedirs(INSTALL_DIR)
 
 # Copy global_vars.py to target location.
 os.system(f"cp {REPO_DIR}/global_vars.py {INSTALL_DIR}/global_vars.py")
+os.system(f"cp {REPO_DIR}/config.yaml {INSTALL_DIR}/config.yaml")
 
 # Create cache directory.
 if not os.path.exists(CACHE_DIR):
