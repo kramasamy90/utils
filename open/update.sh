0e77c183f6b21c093ff0b2dc545930847ee2bd00
First Last <email>
2026-07-04 15:37:53 +0530

    Feat: open/update.sh uses dir from config instead or hard-coded vals.

--- open/update.sh
+++ open/update.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-cp ./src/o.sh ~/.local/opt/open/src/o
-cp ./src/open.py ~/.local/opt/open/src/open.py
-cp ./data/help.txt ~/.local/opt/open/data/help.txt
+FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+REPO_DIR="$( cd "$FILE_DIR/.." &> /dev/null && pwd )"
+CONFIG_FILE="$REPO_DIR/config.yaml"
+
+INSTALL_DIR=$(grep "INSTALL_DIR" "$CONFIG_FILE" | sed -E 's/INSTALL_DIR:\s+(.*)/\1/')
+
+cp "$FILE_DIR/src/o.sh" "$INSTALL_DIR/open/src/o"
+cp "$FILE_DIR/src/open.py" "$INSTALL_DIR/open/src/open.py"
+cp "$FILE_DIR/data/help.txt" "$INSTALL_DIR/open/data/help.txt"
